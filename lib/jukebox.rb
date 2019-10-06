a4d5561824fc46ab856d070dff1e46f13c87c601
Tiffany Tang <tifftang22@gmail.com>
2019-10-06 21:07:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ end
 def play(array)
   puts "Please enter a song name or number:"
   input = gets.strip
-  if (input.to_i).between?(1..9)
+  if (input.to_i).between?(1,9)
     puts "Playing #{array[input-1]}"
   elsif array.include?(input)
     puts "Playing #{array[input.to_i - 1]}"
