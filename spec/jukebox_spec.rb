e8468fe241dcb5a3edc9b78473e54f9ec66b4fe9
Tiffany Tang <tifftang22@gmail.com>
2019-10-06 21:19:17 +0000

    Automatically backed up by Learn

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -82,6 +82,7 @@ describe "CLI Jukebox" do
 
   context "with commands" do
     it "responds to 'exit'" do
+      expect($stdout).to receive(:puts).with(/Please enter a command:/)
       expect($stdout).to receive(:puts).with(/Goodbye/)
       allow(self).to receive(:gets).and_return("exit")
       run(songs)
@@ -110,7 +111,7 @@ describe "CLI Jukebox" do
       expect{ run(songs) }.to output(/1. Phoenix - 1901/).to_stdout
       allow(self).to receive(:gets).and_return("list", "exit")
       expect{ run(songs) }.to output(/9. Amos Lee - Keep It Loose, Keep It Tight/).to_stdout
-
+      
     end
 
     it "responds to 'play'" do
